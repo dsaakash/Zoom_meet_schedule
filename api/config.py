e122916da9e1e3e0e0238d1895fe9817cd8f0d91
savant aakash <savantaakash322@gmail.com>
2022-04-10 13:00:07 +0530

    Created an api file into api package and added config files to add apike and secret_key

---
+++ api/config.py
@@ -0,0 +1,9 @@
+# this file is basically add credentiltials such as API_KEY and API_secret
+
+
+#Definine all api_key and api_secret
+
+
+API_KEY = 'uZlAleG7QNWNtebfcAAKKQ'
+API_SECRET = 'hdc1bkE84b2WnrIIuyt5C8npKdxQErv9jGd8'
+EMAIL_ID = 'divyanshu@intellipaat.com'
